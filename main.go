4ea05c33400382d86dd6c575a7f442b0a7b3a189
Badru <badruakfm@gmail.com>
2026-06-19 07:23:53 +0700

    update port

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
     "fmt"
     "net/http"
+    "os"
 )
 
 func main() {
+    port := os.Getenv("APP_PORT")
+    fmt.Println("Run app in port : " + port)
     http.HandleFunc("/", HelloServer)
-    http.ListenAndServe(":8080", nil)
+    http.ListenAndServe(":" + port, nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
